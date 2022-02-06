6487f41b99e70fb4613144692bccea66c587288c
Claudio Vazquez <claudiojvv10@gmail.com>
2022-02-06 13:44:53 -0600

    Format code

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-#Python
+# Python
 from os import stat
 from typing import Optional
 from enum import Enum
 
-#Pydantic
+# Pydantic
 from pydantic import BaseModel
 from pydantic import Field
 from pydantic import EmailStr
 
-#FastAPI
+# FastAPI
 from fastapi import FastAPI
 from fastapi import status
 from fastapi import Body, Query, Path, Form, Header, Cookie
@@ -19,115 +19,113 @@ app = FastAPI()
 
 # Models
 
-class HairColor(Enum): 
+
+class HairColor(Enum):
     white = "white"
     brown = "brown"
     black = "black"
     blonde = "blonde"
     red = "red"
 
-class Location(BaseModel): 
+
+class Location(BaseModel):
     city: str
     state: str
     country: str
 
+
 class PersonBase(BaseModel):
     first_name: str = Field(
-        ..., 
+        ...,
         min_length=1,
         max_length=50,
         example="Miguel"
-        )
+    )
     last_name: str = Field(
-        ..., 
+        ...,
         min_length=1,
         max_length=50,
         example="Torres"
-        )
+    )
     age: int = Field(
         ...,
         gt=0,
         le=115,
         example=25
     )
-    hair_color: Optional[HairColor] = Field(default=None, example=HairColor.black)
+    hair_color: Optional[HairColor] = Field(
+        default=None, example=HairColor.black)
     is_married: Optional[bool] = Field(default=None, example=False)
 
-class Person(PersonBase): 
+
+class Person(PersonBase):
     password: str = Field(..., min_length=8)
 
-    # class Config: 
-    #     schema_extra = {
-    #         "example": {
-    #             "first_name": "Facundo",
-    #             "last_name": "García Martoni",
-    #             "age": 21, 
-    #             "hair_color": "blonde",
-    #             "is_married": False
-    #         }
-    #     }
 
-class PersonOut(PersonBase): 
-    pass
+class PersonOut(PersonBase):
+    ...
 
-class LoginOut(BaseModel): 
+
+class LoginOut(BaseModel):
     username: str = Field(..., max_length=20, example="miguel2021")
     message: str = Field(default="Login Succesfully!")
 
+
 @app.get(
-    path="/", 
+    path="/",
     status_code=status.HTTP_200_OK
-    )
-def home(): 
+)
+def home():
     return {"Hello": "World"}
 
 # Request and Response Body
 
+
 @app.post(
-    path="/person/new", 
+    path="/person/new",
     response_model=PersonOut,
     status_code=status.HTTP_201_CREATED
-    )
-def create_person(person: Person = Body(...)): 
+)
+def create_person(person: Person = Body(...)):
     return person
 
 # Validaciones: Query Parameters
 
+
 @app.get(
     path="/person/detail",
     status_code=status.HTTP_200_OK
-    )
+)
 def show_person(
     name: Optional[str] = Query(
         None,
-        min_length=1, 
+        min_length=1,
         max_length=50,
         title="Person Name",
         description="This is the person name. It's between 1 and 50 characters",
-        example="Rocío"
-        ),
-    age: str = Query(
-        ...,
-        title="Person Age",
-        description="This is the person age. It's required",
-        example=25
-        )
-): 
+        example="Rocío"),
+        age: str = Query(
+            ...,
+            title="Person Age",
+            description="This is the person age. It's required",
+        example=25)):
     return {name: age}
 
 # Validaciones: Path Parameters
 
+
 @app.get("/person/detail/{person_id}")
 def show_person(
     person_id: int = Path(
-        ..., 
+        ...,
         gt=0,
         example=123
-        )
-): 
+    )
+):
     return {person_id: "It exists!"}
 
 # Validaciones: Request Body
+
 
 @app.put("/person/{person_id}")
 def update_person(
@@ -139,24 +137,22 @@ def update_person(
         example=123
     ),
     person: Person = Body(...),
-    #location: Location = Body(...)
-): 
-    #results = person.dict()
-    #results.update(location.dict())
-    #return results
+):
     return person
 
 # Forms
+
 
 @app.post(
     path="/login",
     response_model=LoginOut,
     status_code=status.HTTP_200_OK
 )
-def login(username: str = Form(...), password: str = Form(...)): 
+def login(username: str = Form(...), password: str = Form(...)):
     return LoginOut(username=username)
 
 # Cookies and Headers Parameters
+
 
 @app.post(
     path="/contact",
@@ -180,5 +176,5 @@ def contact(
     ),
     user_agent: Optional[str] = Header(default=None),
     ads: Optional[str] = Cookie(default=None)
-): 
+):
     return user_agent
